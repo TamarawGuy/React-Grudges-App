eca8ee8a4d6ae52d693cb7a624e3b3b90755cdaa
Viktor <viktor.m.rumenov@gmail.com>
2022-03-19 17:08:54 +0100

    Grudges React App

---
+++ src/Grudge.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { GrudgeContext } from './GrudgeContext';
+
+const Grudge = React.memo(({ grudge }) => {
+  const { toggleForgiveness } = React.useContext(GrudgeContext);
+
+  const forgive = () => toggleForgiveness(grudge.id);
+
+  return (
+    <article className="Grudge">
+      <h3>{grudge.person}</h3>
+      <p>{grudge.reason}</p>
+      <div className="Grudge-c ontrols">
+        <label className="Grudge-forgiven">
+          <input type="checkbox" checked={grudge.forgiven} onChange={forgive} />{' '}
+          Forgiven
+        </label>
+      </div>
+    </article>
+  );
+});
+
+export default Grudge;
